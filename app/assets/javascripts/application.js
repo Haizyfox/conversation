30962aff016a87d5fbf19127896ec7e73857be76
Haizyfox <bradbarrow@gmail.com>
2013-05-30 12:40:38 +1000

    -

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,10 +6,6 @@
 
 //= require angular/app
 //= require angular/states
-//= require_tree ./angular/factories
-//= require_tree ./angular/resources
 //= require_tree ./angular/controllers
-//= require_tree ./angular/directives
-//= require_tree ./angular/filters
 //= require_tree ./angular/states
 //= require_tree ./angular/config
